a3fadb22da846fd2be46768dd3204e6ebed103a4
Viktor Alex Brynjarsson <viktoralex@viktoralex.is>
2019-11-07 17:01:07 +0000

    Test consumer with auto ack

--- test/Owlery.Tests/Models/RabbitConsumer_RecievedEventHandler.cs
+++ test/Owlery.Tests/Models/RabbitConsumer_RecievedEventHandler.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using Owlery.Models;
@@ -66,6 +67,56 @@ namespace Owlery.Tests.Models
             // THEN
             var service = serviceCollection.GetRequiredService<ConsumerController>();
             Assert.True(service.ConsumeCalled);
+        }
+
+        [Fact]
+        public void ShouldNotAckOrNackOnAutoAcknowledgement()
+        {
+            // GIVEN - An autoAck consumer
+            var serviceCollectionFactory = new ServiceCollection();
+            // Make singleton so we can assert that the method ran
+            serviceCollectionFactory.AddSingleton<ConsumerController>();
+
+            var serviceCollection = serviceCollectionFactory.BuildServiceProvider();
+
+            var consumerMethod = new ConsumerMethod(
+                typeof(ConsumerController).GetMethod("Consume"),
+                typeof(ConsumerController),
+                new RabbitConsumerAttribute(
+                    queueName: "consumerQueue",
+                    acknowledgementType: AcknowledgementType.AutoAck),
+                null
+            );
+
+            var mockModel = new Mock<IModel>();
+            var logger = TestLogger.CreateXUnit<RabbitConsumer>(this.output);
+
+            var eventArgs = new BasicDeliverEventArgs();
+
+            // WHEN - The consumer consumes a message
+            var rabbitConsumer = new RabbitConsumer(
+                consumerMethod,
+                mockModel.Object,
+                serviceCollection,
+                logger
+            );
+            rabbitConsumer.RecievedEventHandler(null, eventArgs);
+
+            // THEN - Neither BasicAck nor BasicNack should be called
+            var service = serviceCollection.GetRequiredService<ConsumerController>();
+            Assert.True(service.ConsumeCalled);
+
+            mockModel.Verify(m => m.BasicConsume(
+                    It.IsAny<string>(),
+                    It.IsAny<bool>(),
+                    It.IsAny<string>(),
+                    It.IsAny<bool>(),
+                    It.IsAny<bool>(),
+                    It.IsAny<IDictionary<string, object>>(),
+                    It.IsAny<EventingBasicConsumer>()
+                )
+            );
+            mockModel.VerifyNoOtherCalls();
         }
     }
 }
