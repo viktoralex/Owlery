f193c1e84a1ffdab2c2f6033e9b9dee256bf5a02
Arnþór Hjaltason <arnthor@vodafone.is>
2020-01-09 15:39:54 +0000

    Feature: Adding basicProperties to RabbitServices Publish

--- src/Owlery/Owlery/Services/IRabbitService.cs
+++ src/Owlery/Owlery/Services/IRabbitService.cs
@@ -1,7 +1,9 @@
+using RabbitMQ.Client;
+
 namespace Owlery.Services
 {
     public interface IRabbitService
     {
-         void Publish(string routingKey, object body, string exchange = null);
+         void Publish(string routingKey, object body, string exchange = null, IBasicProperties basicProperties = null);
     }
 }
