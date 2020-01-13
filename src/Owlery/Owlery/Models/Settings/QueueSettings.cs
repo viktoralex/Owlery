ee6bc09c918674b1f20b7ee23245cd4a019d3b18
Viktor Alex Brynjarsson <viktoralex@viktoralex.is>
2020-01-13 13:21:35 +0000

    Make queues non-exclusive by default

--- src/Owlery/Owlery/Models/Settings/QueueSettings.cs
+++ src/Owlery/Owlery/Models/Settings/QueueSettings.cs
@@ -6,7 +6,7 @@ namespace Owlery.Models.Settings
     {
         public string QueueName { get; set; }
         public bool Durable { get; set; } = false;
-        public bool Exclusive { get; set; } = true;
+        public bool Exclusive { get; set; } = false;
         public bool AutoDelete { get; set; } = true;
         public string DeadLetterRoutingKey { get; set; } = null;
         public string DeadLetterExchange { get; set; } = null;
